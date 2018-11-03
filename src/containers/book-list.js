1a60bfa17c41fa85e7204ec5e3917109f609837e
adam <atomicpenguines@gmail.com>
2018-11-03 21:19:40 +0000

    book-list container updated

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-export class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
@@ -17,3 +17,17 @@ export class BookList extends Component {
     return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+// When state changes this rerenders
+// takes in application state and returns props
+// links redux to react
+function mapStateToProps(state) {
+  // whatever is return will show up as props inside BookList
+  // whatever is returned here is set to this.props of the component
+  return {
+    books: state.books
+  };
+}
+
+// export the container, two calls
+export default connect(mapStateToProps)(BookList);
