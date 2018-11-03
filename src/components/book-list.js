d99b90eb990651194b92417adc4b4f4d11715c9c
adam <atomicpenguines@gmail.com>
2018-11-03 17:19:23 +0000

    returning a piece of state

---
+++ src/components/book-list.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+export class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+  }
+}
