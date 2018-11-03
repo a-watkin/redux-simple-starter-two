a7d32e17f413ee76f46a4caf9639e13663f31d9e
adam <atomicpenguines@gmail.com>
2018-11-03 21:47:09 +0000

    some notes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import BooksReducer from "./reducer_books";
 
 // tells redux how to create application state
+// add the global state books
 const rootReducer = combineReducers({
   books: BooksReducer
 });
