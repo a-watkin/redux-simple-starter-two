f6a082bf18aba8d4708b2e21ca131c90bb8cb050
adam <atomicpenguines@gmail.com>
2018-11-03 22:21:16 +0000

    aded selectBook and made it available to reducers, also added notes

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+// ensures the actuion flows through all the reducers
+import { bindActionCreators } from "redux";
 
 class BookList extends Component {
   renderList() {
@@ -29,5 +32,18 @@ function mapStateToProps(state) {
   };
 }
 
+// anything returned from this function ends up as props on BookList container
+function mapDispatchToProps(disaptch) {
+  // the value selectBook is the action imported above, so that function is called whenever the key is evaluated
+
+  // the result of selectBook passes throuh all the reducers
+  return bindActionCreators({ selectBook: selectBook }, disaptch);
+}
+
 // export the container, two calls
-export default connect(mapStateToProps)(BookList);
+
+// promote BookList from a component to a container, let it know about this new distpatch method, SelectBook, make it available as a prop via mapDispatchToProps
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList);
