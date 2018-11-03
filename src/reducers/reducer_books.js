3bbf7e2d3716252ed81a10161f00d64ce7128579
adam <atomicpenguines@gmail.com>
2018-11-03 05:07:00 +0000

    added books reducer and notes

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: the good parts" },
+    { title: "Eloquent Javascript" },
+    { title: "Javascript applications" },
+    { title: "I Robot" }
+  ];
+}
