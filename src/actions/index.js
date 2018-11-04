8796eb592a4c339fefd54d645b6ab0f9259193e9
adam <atomicpenguines@gmail.com>
2018-11-04 00:32:28 +0000

    adding active book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export function selectBook(book) {
-  console.log("A book has been selected: ", book);
+  //   console.log("A book has been selected: ", book);
+
   // select book is an action creator and needs to return an action
   // an object with a type property
+
+  // this object is automatically sent to all reducers
   return {
     type: "BOOK_SELECTED",
     payload: book
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,5 @@
+// reducers are always called by actions
+
+// the state here is only for the part of the state it is responsible for
+// it is not the overall application state
+export default function(state, action) {}
